93232d7c95bbbd1b485062631a3045cc59da36da
Ghani <ghaniahmed951@gmail.com>
2020-12-28 18:14:09 +0500

    ReduxStore+axios+privateRoute HOC

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './container/Home';
 import Signin from './container/Signin';
 import Signup from './container/Signup';
+import PrivateRoute from './HOC/PrivateRoute';
 
 function App() {
   return (
     <>
      <Router>
        <Switch>
-         <Route path='/' exact component={ Home }/>
+         <PrivateRoute path='/' exact component={ Home }/>
          <Route path='/signin' component={ Signin }/>
          <Route path='/signup' component={ Signup } />
        </Switch>
